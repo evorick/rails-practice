5021538864190b3a69cb3b609d5fb3078fdf3b57
evorick <rickkadlac@Ricks-MacBook-Pro.local>
2015-08-13 10:56:21 -0500

    Added validation

--- music_db/app/models/song.rb
+++ music_db/app/models/song.rb
@@ -1,3 +1,23 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  validates :name, presence: true, length: {minimum: 1}, uniqueness: true
+  validates_uniqueness_of :name, :scope => :artist_id
+
+  scope :recent, -> (minutes_past = 60) {where("created_at > ?", minutes_past.minutes.ago)}
 end
+
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  artist_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_songs_on_artist_id  (artist_id)
+#
