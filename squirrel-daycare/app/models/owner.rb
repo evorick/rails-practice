cbb57b96a58c65093f457562dab8413e257f5b3e
evorick <rickkadlac@Ricks-MacBook-Pro.local>
2015-08-15 22:45:54 -0500

    added validation

--- squirrel-daycare/app/models/owner.rb
+++ squirrel-daycare/app/models/owner.rb
@@ -1,3 +1,8 @@
 class Owner < ActiveRecord::Base
   has_many :squirrels
+
+  validates :name, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
+
+  scope :recent, -> (minutes_past = 60) {where("created_at > ?", minutes_past.minutes.ago)}
 end
--- squirrel-daycare/app/models/squirrel.rb
+++ squirrel-daycare/app/models/squirrel.rb
@@ -1,3 +1,13 @@
 class Squirrel < ActiveRecord::Base
   belongs_to :owner
+  
+  validates :name, presence: true, uniqueness: true
+
+  validates :breed, presence: true
+
+  validates :age, presence: true
+
+  validates :owner_id, presence: true
+
+  scope :recent, -> (minutes_past = 60) {where("created_at > ?", minutes_past.minutes.ago)}
 end
