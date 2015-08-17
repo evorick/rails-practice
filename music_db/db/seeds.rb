8b4f30e7e88aced9a50b2f46a22bb9881ef26df2
evorick <rickkadlac@Ricks-MacBook-Pro.local>
2015-08-17 08:46:46 -0500

    Added seeds for genres

--- music_db/db/seeds.rb
+++ music_db/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Genre.create(name: "pop")
+Genre.create(name: "country")
+Genre.create(name: "hip-hop")
+Genre.create(name: "indie")
+Genre.create(name: "jazz")
